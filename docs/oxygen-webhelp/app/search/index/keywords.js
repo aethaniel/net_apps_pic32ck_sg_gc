6441ecea396e302d07ae2ed95325405401a825cf
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-07-28 10:59:03 +0530

    [MH3-83489] Added Demo Documentation for PIC32CK GC01

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,16 @@
+define(function() {var keywords=[{w:"1.2",p:["p0"]},{w:"TCP/IP",p:["p0","p1","p4","p5","p6"]},{w:"TCP",p:["p0","p1"]},{w:"Server",p:["p0","p6"]},{w:"1.1",p:["p1"]},{w:"Client",p:["p1"]},{w:"2.2",p:["p2"]},{w:"MPLAB",p:["p2","p3"]},{w:"Code",p:["p2"]},{w:"Configurator",p:["p2"]},{w:"2.1",p:["p3"]},{w:"X",p:["p3"]},{w:"Integrated",p:["p3"]},{w:"Development",p:["p3"]},{w:"Environment",p:["p3"]},{w:"1",p:["p4"]},{w:"Harmony",p:["p4"]},{w:"3",p:["p4"]},{w:"Application",p:["p4","p5"]},{w:"for",p:["p4"]},{w:"PIC32CK",p:["p4"]},{w:"GC01",p:["p4"]},{w:"Family",p:["p4"]},{w:"1.4",p:["p5"]},{w:"WEB",p:["p5","p6"]},{w:"NET",p:["p5","p6"]},{w:"Super",p:["p5"]},{w:"Set",p:["p5"]},{w:"SD",p:["p5","p6"]},{w:"Card",p:["p5","p6"]},{w:"FATFS",p:["p5","p6"]},{w:"1.3",p:["p6"]},{w:"2",p:["p7"]},{w:"Appendix",p:["p7"]},{w:"Context",p:["p8"]},{w:"Sensitive",p:["p8"]},{w:"Help",p:["p8"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3];
+ph["p1"]=[4, 1, 2, 5];
+ph["p2"]=[6, 7, 8, 9];
+ph["p3"]=[10, 7, 11, 12, 13, 14];
+ph["p4"]=[15, 16, 17, 1, 18, 19, 20, 21, 22];
+ph["p5"]=[23, 1, 24, 25, 26, 27, 28, 29, 30, 18];
+ph["p6"]=[31, 1, 24, 25, 3, 28, 29, 30];
+ph["p7"]=[32, 33];
+ph["p8"]=[34, 35, 36];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {0:4,3:7,2:7,6:4,4:-1,7:-1,1:4,5:4};
+});

e4ca93489a7af06fedd4a253006a235f22461deb
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-08-04 12:31:38 +0530

    [MH3-83489] Updated Demo Documentation for PIC32CK GC01

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20230801090459';
+                args = '20230804063433';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
